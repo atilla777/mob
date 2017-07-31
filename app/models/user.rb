e804333e99f2a374d045e915a1fa1f0e0104973c
Aleksey Slivka <slivka77@inbox.ru>
2017-07-31 12:25:15 +0300

    Devise was added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable #, :omniauthable
+
   #validates :name, length: {min: 3, max: 300}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'posts#index'
+
+  resources :posts, only: [:index]
+
+  #get '', to.'session' as: :login
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
