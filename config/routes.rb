7dbc6865df49c5527def892814b4dcc15e930c03
Aleksey Slivka <slivka77@inbox.ru>
2017-08-08 09:38:23 +0300

    Translate for Vote and Picture

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   require 'sidekiq/cron/web'
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-
   authenticate :user, lambda { |u| u.admin? } do
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
     mount Sidekiq::Web => '/sidekiq'
   end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-#  devise_scope :user do
-#    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-#  end
 
   root to: 'posts#index'
 
